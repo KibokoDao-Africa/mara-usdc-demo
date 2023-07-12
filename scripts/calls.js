1e58120d2bcf3225436d7e46a653f21c4450ce73
timothyAgevi <timothyagevi@gmail.com>
2023-07-12 20:59:09 +0300

    set env variables

--- scripts/calls.js
+++ scripts/calls.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 
 const { Contract } = require("hardhat/internal/hardhat-network/stack-traces/model");
-const contractAddress = "0x064004473A2BB2F9A1fF07230cEbAc245f2Fecbc";
+const contractAddress =process.env.CONTRACT_ADDRESS
 const contractName ="Mara";
 async function main(){
 
