49ae6ab4f4a7a8d5e9996adf1cc7660aac28ec86
mmulika <mulikamike@gmail.com>
2023-07-19 15:49:10 +0300

    CreateStudent

--- scripts/calls.js
+++ scripts/calls.js
@@ -11,15 +11,19 @@ async function main(){
     
 
     const receipt = await tx.wait();
+
     const addmember =await call.creatMember(10,"mii");
+    
+    const addStudent = await  call.createStudent(11,"kiboko","0xFf897981d299e1668C9B0CfbE693A93549d92c26")
 
 
     const  Count = await call.getCount;
-    console.log(addmember);
-    console.log(Count);
+    // console.log(addmember);
+    // console.log(Count);
 
-    console.log(receipt);
+    // console.log(receipt);
 
+     console.log(addStudent);
 
 
 
